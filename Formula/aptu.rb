5174e905b25fed063fc02dc8c328bc19b61dce0c
Hugues Clouatre <hugues@linux.com>
2025-12-23 06:22:08 +0000

    Update aptu formula for v0.1.3
    Signed-off-by: Hugues Clouatre <hugues@linux.com>

--- Formula/aptu.rb
+++ Formula/aptu.rb
@@ -1,18 +1,18 @@
 class Aptu < Formula
   desc "Gamified OSS issue triage with AI assistance"
   homepage "https://github.com/clouatre-labs/aptu"
-  version "0.1.2"
+  version "0.1.3"
   license "Apache-2.0"
   
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/clouatre-labs/aptu/releases/download/v0.1.2/aptu-aarch64-apple-darwin.tar.gz"
-    sha256 "a3b1c08b49ced8120c134e6642fbdf128bc7be217fa5e8e441b1d46afc95515d"
+    url "https://github.com/clouatre-labs/aptu/releases/download/v0.1.3/aptu-aarch64-apple-darwin.tar.gz"
+    sha256 "86401b8e4161fc716449612bd81289ab9723eb34b3858046a758e66ae67d006e"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/clouatre-labs/aptu/releases/download/v0.1.2/aptu-aarch64-unknown-linux-musl.tar.gz"
-    sha256 "8865a36873520379fc04b0d73490cd7c5157d019cfbcbdf88e1a4c0e84f7415b"
+    url "https://github.com/clouatre-labs/aptu/releases/download/v0.1.3/aptu-aarch64-unknown-linux-musl.tar.gz"
+    sha256 "f87afaf3f390aacb621a0a333d3a606495e504d0c3380002ca6841d52826a63e"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/clouatre-labs/aptu/releases/download/v0.1.2/aptu-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "321c13de1163b6e9b84ba472ba4f9c29b142386651bbae87962947f3a1669fa1"
+    url "https://github.com/clouatre-labs/aptu/releases/download/v0.1.3/aptu-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "9169e1f6aeb77f0a5c795d3a6205576fa838148db8c11eccbe5b08e5319059c7"
   end
   
   def install
