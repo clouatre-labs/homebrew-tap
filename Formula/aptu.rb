306202d4bde0f2f676fcb21ad00d4c208498f5bc
Hugues Clouatre <hugues@linux.com>
2026-01-03 03:48:57 +0000

    Update aptu formula for v0.2.13
    Signed-off-by: Hugues Clouatre <hugues@linux.com>

--- Formula/aptu.rb
+++ Formula/aptu.rb
@@ -4,14 +4,14 @@ class Aptu < Formula
   license "Apache-2.0"
   
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/clouatre-labs/aptu/releases/download/v0.2.12/aptu-0.2.12-aarch64-apple-darwin.tar.gz"
-    sha256 "2c7672940f2f7142962ad049591380f7c18b3da508eb64f466d40350a83d1fe0"
+    url "https://github.com/clouatre-labs/aptu/releases/download/v0.2.13/aptu-0.2.13-aarch64-apple-darwin.tar.gz"
+    sha256 "6204f1752cc2ddf19e35e23ee78404ca60ba2374a2e295040bbb1cbbcf853e84"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/clouatre-labs/aptu/releases/download/v0.2.12/aptu-0.2.12-aarch64-unknown-linux-musl.tar.gz"
-    sha256 "a98e610e7a68f58d424d14b34072d21204af9bc32a5375eec2065a90ab56765a"
+    url "https://github.com/clouatre-labs/aptu/releases/download/v0.2.13/aptu-0.2.13-aarch64-unknown-linux-musl.tar.gz"
+    sha256 "bc04e7202d8466ecc3e6a22a1c50f052e68811050d0cbf08db577ddd0e622bc6"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/clouatre-labs/aptu/releases/download/v0.2.12/aptu-0.2.12-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "fec4e1f936add5b76566a8b58b9dbe97fd3a1b474e387ebce62c86fce3c06f13"
+    url "https://github.com/clouatre-labs/aptu/releases/download/v0.2.13/aptu-0.2.13-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "d3e06d4b2c1deb8fef9c448ae9d05caaf31f418fc3d002b546ab8ba87edda174"
   end
   
   def install
