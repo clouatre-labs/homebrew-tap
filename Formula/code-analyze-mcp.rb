4d5e93dab4e4af10ddc8897ef96907952cbae3be
Hugues Clouâtre <15008125+clouatre@users.noreply.github.com>
2026-03-27 19:28:16 -0400

    Update code-analyze-mcp formula for v0.1.10 (#48)
    Signed-off-by: Hugues Clouatre <hugues@linux.com>

--- Formula/code-analyze-mcp.rb
+++ Formula/code-analyze-mcp.rb
@@ -5,13 +5,13 @@ class CodeAnalyzeMcp < Formula
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/clouatre-labs/code-analyze-mcp/releases/download/v0.1.10/code-analyze-mcp-0.1.10-aarch64-apple-darwin.tar.gz"
-    sha256 "760bc038577f79447b230b9413bdea208e93d103e7160913f7c54182bdc42eef"
+    sha256 "c9a692f9773ccfb50fac11ca7711905f22c3d54528fa95ed357724309d3ca1ff"
   elsif OS.linux? && Hardware::CPU.arm?
     url "https://github.com/clouatre-labs/code-analyze-mcp/releases/download/v0.1.10/code-analyze-mcp-0.1.10-aarch64-unknown-linux-musl.tar.gz"
-    sha256 "da1444ef4fd1da99ebca17185ecd948dff40306dcc9c0f7bbec4cbf5e7651471"
+    sha256 "a7486430b570a945dba7a3c14e53dc7e4a67504658316b46b6edf5011ad41afe"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/clouatre-labs/code-analyze-mcp/releases/download/v0.1.10/code-analyze-mcp-0.1.10-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "34473335ee4d3cb079315cfb4fdb29c3ebfe68cb5ee8b1d857afc7a82c8a7478"
+    sha256 "c4d9dd3048099a28508c19b6c4771fd642b2d4e37165c57ba598ccb6c09f9844"
   end
 
   def install
