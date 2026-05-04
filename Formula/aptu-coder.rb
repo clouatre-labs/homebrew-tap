8ca52f68075f2ee6639f7dcee5926a950bc7850f
Hugues Clouatre <hugues@linux.com>
2026-05-04 00:08:22 +0000

    Update aptu-coder formula for v0.8.0
    Signed-off-by: Hugues Clouatre <hugues@linux.com>

--- Formula/aptu-coder.rb
+++ Formula/aptu-coder.rb
@@ -4,14 +4,14 @@ class AptuCoder < Formula
   license "Apache-2.0"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/clouatre-labs/aptu-coder/releases/download/v0.7.0/aptu-coder-0.7.0-aarch64-apple-darwin.tar.gz"
-    sha256 "ff47d404774651c1196711866fa1c1c3a3a1c4961b20bc511465a2371ba5dcfc"
+    url "https://github.com/clouatre-labs/aptu-coder/releases/download/v0.8.0/aptu-coder-0.8.0-aarch64-apple-darwin.tar.gz"
+    sha256 "3fec1bbc673c3122f8f6b5794c233b0c31d9c4e62dc6bf185ab82cd5a6536a05"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/clouatre-labs/aptu-coder/releases/download/v0.7.0/aptu-coder-0.7.0-aarch64-unknown-linux-musl.tar.gz"
-    sha256 "ed097bb8db2b957bfd0f206e129856034b2f05070e14e3f71cf702f8121e1560"
+    url "https://github.com/clouatre-labs/aptu-coder/releases/download/v0.8.0/aptu-coder-0.8.0-aarch64-unknown-linux-musl.tar.gz"
+    sha256 "0e9256611ba3ac40899ed6b9f264e9a05ab07bde3fd99ce42c90f12223c072fd"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/clouatre-labs/aptu-coder/releases/download/v0.7.0/aptu-coder-0.7.0-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "88817644eeb390cc2623006434b50347921535746015920530279c78a62f3011"
+    url "https://github.com/clouatre-labs/aptu-coder/releases/download/v0.8.0/aptu-coder-0.8.0-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "a127d80f489ab14d48add4cecc50c51ea73e7a932ddb6b7e721f1963d04eb51e"
   end
 
   def install
