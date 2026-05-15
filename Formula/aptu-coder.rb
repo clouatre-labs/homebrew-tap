12b76ed1bc0a637ff08760e965eb46aed4796c72
Hugues Clouâtre <hugues@linux.com>
2026-05-15 16:20:43 -0400

    fix(formula): restore service do block dropped by automated bump
    Signed-off-by: Hugues Clouâtre <hugues@linux.com>

--- Formula/aptu-coder.rb
+++ Formula/aptu-coder.rb
@@ -14,6 +14,13 @@ class AptuCoder < Formula
     sha256 "d6d793a44bcfddeb033885185cafbac18c017a72cd0f141942e961ac7fd7758d"
   end
 
+  service do
+    run [opt_bin/"aptu-coder", "--port", "49200"]
+    keep_alive true
+    log_path var/"log/aptu-coder.log"
+    error_log_path var/"log/aptu-coder.log"
+  end
+
   def install
     bin.install "aptu-coder"
   end
