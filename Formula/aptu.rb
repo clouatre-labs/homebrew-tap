e7a826914b68afa2bc366d6189fafc59e27f5fdc
Hugues Clouâtre <hugues@linux.com>
2026-09-13 08:30:19 -0400

    Update aptu formula for v0.10.19 (#242)
    Signed-off-by: Hugues Clouatre <hugues@linux.com>

--- Formula/aptu.rb
+++ Formula/aptu.rb
@@ -4,14 +4,14 @@ class Aptu < Formula
   license "Apache-2.0"
   
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/clouatre-labs/aptu/releases/download/v0.10.18/aptu-cli-0.10.18-aarch64-apple-darwin.tar.gz"
-    sha256 "5f678851bbc3930b2281e3f79cb97e028422e437981c28171e2e12e57de9c78f"
+    url "https://github.com/clouatre-labs/aptu/releases/download/v0.10.19/aptu-cli-0.10.19-aarch64-apple-darwin.tar.gz"
+    sha256 "8837f73534d1336842a346926a20e040802d7591ddea89838e61e0dcede0e2db"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/clouatre-labs/aptu/releases/download/v0.10.18/aptu-cli-0.10.18-aarch64-unknown-linux-musl.tar.gz"
-    sha256 "05b35332f6727606b2406a8a91117d57cf00c1821653e2f0795a45904464b83d"
+    url "https://github.com/clouatre-labs/aptu/releases/download/v0.10.19/aptu-cli-0.10.19-aarch64-unknown-linux-musl.tar.gz"
+    sha256 "b33ecd89ba9ea450a7331d145ead55779781ac4c3bd5ea8a0ba8952b213577f8"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/clouatre-labs/aptu/releases/download/v0.10.18/aptu-cli-0.10.18-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "4d5e75b9a185191177f5bb382aa288322c18d405d0458d7bd84a90dbcb936bff"
+    url "https://github.com/clouatre-labs/aptu/releases/download/v0.10.19/aptu-cli-0.10.19-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "3fb59a6e3fc63371233c13416b372edc66427bb13ce8867482948993fd89344a"
   end
   
   def install
